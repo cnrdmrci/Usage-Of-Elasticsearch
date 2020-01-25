cc85833e3c10e9cdc87d9d1b07da42e1e5fb0cd4
cnrdmrci <canerdemirci.mail@gmail.com>
2020-01-25 23:42:20 +0300

    createIndex function is generic

--- DocumentApi/SingleDocumenApi/IndexApi.cs
+++ DocumentApi/SingleDocumenApi/IndexApi.cs
@@ -9,6 +9,28 @@ namespace Usage_Of_Elasticsearch.DocumentApi
     public class IndexApi
     {
         private ElasticClient elasticClient = ElasticInstanceCreator.GetElasticClientInstance();
+        public bool CreateIndex<T>(int numberOfShards, int numberOfReplicas, string indexName, string aliasName = null) where T : class
+        {
+            if (!checkIndexExists(indexName))
+            {
+                var indexSettings = new IndexSettings();
+                indexSettings.NumberOfReplicas = numberOfReplicas;
+                indexSettings.NumberOfShards = numberOfShards;
+
+                var createIndexDescriptor = new CreateIndexDescriptor(indexName)
+                    .Map<T>(m => m.AutoMap())
+                    .InitializeUsing(new IndexState() { Settings = indexSettings });
+
+                if(!String.IsNullOrWhiteSpace(aliasName))
+                    createIndexDescriptor.Aliases(a => a.Alias(aliasName));
+
+                CreateIndexResponse response = elasticClient.Indices.Create(createIndexDescriptor);
+                return response.Acknowledged;
+            }
+
+            return true;
+        }
+
         public bool CreateIndex(int numberOfShards, int numberOfReplicas, string indexName, string aliasName = null)
         {
             if (!checkIndexExists(indexName))
@@ -17,12 +39,10 @@ namespace Usage_Of_Elasticsearch.DocumentApi
                 indexSettings.NumberOfReplicas = numberOfReplicas;
                 indexSettings.NumberOfShards = numberOfShards;
 
-                
                 var createIndexDescriptor = new CreateIndexDescriptor(indexName)
-                    //.Map< whichClassMapped? >(m => m.AutoMap())
                     .InitializeUsing(new IndexState() { Settings = indexSettings });
 
-                if(!String.IsNullOrWhiteSpace(aliasName))
+                if (!String.IsNullOrWhiteSpace(aliasName))
                     createIndexDescriptor.Aliases(a => a.Alias(aliasName));
 
                 CreateIndexResponse response = elasticClient.Indices.Create(createIndexDescriptor);
