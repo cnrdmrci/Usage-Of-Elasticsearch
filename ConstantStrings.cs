5ab7fd9ff7beecde2d3d985cd2d632a8aae83fd1
cnrdmrci <canerdemirci.mail@gmail.com>
2020-01-25 23:27:07 +0300

    all of constant storage

---
+++ ConstantStrings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Usage_Of_Elasticsearch
+{
+    public static class ConstantStrings
+    {
+        public const string IndexName = "user_information";
+    }
+}
