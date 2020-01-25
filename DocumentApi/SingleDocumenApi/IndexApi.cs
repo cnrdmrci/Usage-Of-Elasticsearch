62d4781db412cfe40a5a988e7905e4e13b25df21
cnrdmrci <canerdemirci.mail@gmail.com>
2020-01-25 23:07:43 +0300

    CreateIndex function created.

--- DocumentApi/SingleDocumenApi/IndexApi.cs
+++ DocumentApi/SingleDocumenApi/IndexApi.cs
@@ -2,10 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Nest;
 
 namespace Usage_Of_Elasticsearch.DocumentApi
 {
     public class IndexApi
     {
+        private ElasticClient elasticClient = ElasticInstanceCreator.GetElasticClientInstance();
+        public bool CreateIndex(int numberOfShards, int numberOfReplicas, string indexName, string aliasName = null)
+        {
+            if (!checkIndexExists(indexName))
+            {
+                var indexSettings = new IndexSettings();
+                indexSettings.NumberOfReplicas = numberOfReplicas;
+                indexSettings.NumberOfShards = numberOfShards;
+
+                
+                var createIndexDescriptor = new CreateIndexDescriptor(indexName)
+                    //.Map< whichClassMapped? >(m => m.AutoMap())
+                    .InitializeUsing(new IndexState() { Settings = indexSettings });
+
+                if(!String.IsNullOrWhiteSpace(aliasName))
+                    createIndexDescriptor.Aliases(a => a.Alias(aliasName));
+
+                CreateIndexResponse response = elasticClient.Indices.Create(createIndexDescriptor);
+                return response.Acknowledged;
+            }
+
+            return true;
+        }
+
+        private bool checkIndexExists(string indexName)
+        {
+            return elasticClient.Indices.Exists(indexName).Exists;
+        }
     }
 }
