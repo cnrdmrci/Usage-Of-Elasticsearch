447d8f123a33c56949cfd74ceb1fccc6c9f5da28
cnrdmrci <canerdemirci.mail@gmail.com>
2020-01-25 19:25:52 +0300

    created MultiDocumentApi

---
+++ DocumentApi/MultiDocumentApi/MultiGetApi.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Usage_Of_Elasticsearch.DocumentApi.MultiDocumentApi
+{
+    public class MultiGetApi
+    {
+    }
+}
