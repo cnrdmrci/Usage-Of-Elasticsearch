631b006222eace199b04812e864a4e79153b54fd
cnrdmrci <canerdemirci.mail@gmail.com>
2020-01-25 23:26:26 +0300

    Model folder created

---
+++ Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Usage_Of_Elasticsearch
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+    }
+}
