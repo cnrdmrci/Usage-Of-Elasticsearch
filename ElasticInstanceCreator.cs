ecab42b79c933535f79701ac54a5218ad3f67e6c
cnrdmrci <canerdemirci.mail@gmail.com>
2020-01-25 19:30:05 +0300

    created ElasticInstanceCreator

---
+++ ElasticInstanceCreator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Usage_Of_Elasticsearch
+{
+    public static class ElasticInstanceCreator
+    {
+        public static void GetInstance()
+        {
+
+        }
+    }
+}
