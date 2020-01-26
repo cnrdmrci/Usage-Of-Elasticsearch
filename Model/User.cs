911d4d8ff25914acc67f05d95fbe525e6725e881
cnrdmrci <canerdemirci.mail@gmail.com>
2020-01-26 18:16:11 +0300

    id added

--- Model/User.cs
+++ Model/User.cs
@@ -7,6 +7,7 @@ namespace Usage_Of_Elasticsearch
 {
     public class User
     {
+        public int Id { get; set; }
         public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
