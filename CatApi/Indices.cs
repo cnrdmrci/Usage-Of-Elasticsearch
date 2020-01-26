492b4b1c8b7cb5415a1b6f5a6b7d3c2539675d6a
cnrdmrci <canerdemirci.mail@gmail.com>
2020-01-26 21:46:28 +0300

    CatApi is created.

---
+++ CatApi/Indices.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nest;
+
+namespace Usage_Of_Elasticsearch.CatApi
+{
+    public class Indices
+    {
+        private ElasticClient elasticClient = ElasticInstanceCreator.GetElasticClientInstance();
+
+        public async Task<List<string>> GetAllIndexName()
+        {
+            var response = await elasticClient.Cat.IndicesAsync();
+
+            var indexList = response.Records.Select(x => x.Index).ToList();
+            return indexList;
+        }
+    }
+}
---
+++ Controllers/CatApiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Usage_Of_Elasticsearch.CatApi;
+
+namespace Usage_Of_Elasticsearch.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CatApiController : ControllerBase
+    {
+        [HttpGet("GetllAllIndexName")]
+        public async Task<IActionResult> GetllAllIndexName()
+        {
+            Indices indices = new Indices();
+            var response = await indices.GetAllIndexName();
+            
+            return Ok(response);
+        }
+    }
+}
