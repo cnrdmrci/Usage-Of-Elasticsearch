af4dd3a08b7c70682240151da30cc244e4a04537
cnrdmrci <canerdemirci.mail@gmail.com>
2020-01-26 22:44:06 +0300

    Aggregations are completed.

---
+++ Aggregations/Aggregation.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nest;
+
+namespace Usage_Of_Elasticsearch.Aggregations
+{
+    public class Aggregation
+    {
+        private ElasticClient elasticClient = ElasticInstanceCreator.GetElasticClientInstance();
+
+        public async Task<double> GetMinValueAsync<T>(string indexName) where T : class
+        {
+            var response = await elasticClient.SearchAsync<T>(s => s
+                .Index(indexName)
+                .Query(q=>q
+                    .MatchAll())
+                .Size(0)
+                .Aggregations(ag => ag
+                    .Min("age",s => s.Field("age")))
+            );
+
+            return response.Aggregations.Min("age")?.Value.Value ?? -1;
+        }
+        
+        public async Task<double> GetMaxValueAsync<T>(string indexName) where T : class
+        {
+            var response = await elasticClient.SearchAsync<T>(s => s
+                .Index(indexName)
+                .Query(q=>q
+                    .MatchAll())
+                .Size(0)
+                .Aggregations(ag => ag
+                    .Max("age",s => s.Field("age")))
+            );
+
+            return response.Aggregations.Min("age")?.Value.Value ?? -1;
+        }
+
+        public async Task<double> GetSumValueAsync<T>(string indexName) where T : class
+        {
+            var response = await elasticClient.SearchAsync<T>(s => s
+                .Index(indexName)
+                .Query(q => q
+                    .MatchAll())
+                .Size(0)
+                .Aggregations(ag => ag
+                    .Sum("age", s => s.Field("age")))
+            );
+
+            return response.Aggregations.Min("age")?.Value.Value ?? -1;
+        }
+
+        public async Task<double> GetAverageValueAsync<T>(string indexName) where T : class
+        {
+            var response = await elasticClient.SearchAsync<T>(s => s
+                .Index(indexName)
+                .Query(q => q
+                    .MatchAll())
+                .Size(0)
+                .Aggregations(ag => ag
+                    .Average("age", s => s.Field("age")))
+            );
+
+            return response.Aggregations.Min("age")?.Value.Value ?? -1;
+        }
+
+        public async Task<StatsAggregate> GetStatsValueAsync<T>(string indexName) where T : class
+        {
+            var response = await elasticClient.SearchAsync<T>(s => s
+                .Index(indexName)
+                .Query(q => q
+                    .MatchAll())
+                .Size(0)
+                .Aggregations(ag => ag
+                    .Stats("age", s => s.Field("age")))
+            );
+
+            return response.Aggregations.Stats("age");
+        }
+    }
+}
---
+++ Controllers/AggregationController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Usage_Of_Elasticsearch.Aggregations;
+
+namespace Usage_Of_Elasticsearch.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AggregationController : ControllerBase
+    {
+        [HttpGet("GetMinAge")]
+        public async Task<IActionResult> GetMinAge()
+        {
+            Aggregation aggregation = new Aggregation();
+            var response = await aggregation.GetMinValueAsync<User>(ConstantStrings.IndexName);
+
+            return Ok(response);
+        }
+
+        [HttpGet("GetMaxAge")]
+        public async Task<IActionResult> GetMaxAge()
+        {
+            Aggregation aggregation = new Aggregation();
+            var response = await aggregation.GetMaxValueAsync<User>(ConstantStrings.IndexName);
+
+            return Ok(response);
+        }
+
+        [HttpGet("GetSumAge")]
+        public async Task<IActionResult> GetSumAge()
+        {
+            Aggregation aggregation = new Aggregation();
+            var response = await aggregation.GetSumValueAsync<User>(ConstantStrings.IndexName);
+
+            return Ok(response);
+        }
+
+        [HttpGet("GetAverageAge")]
+        public async Task<IActionResult> GetAverageAge()
+        {
+            Aggregation aggregation = new Aggregation();
+            var response = await aggregation.GetAverageValueAsync<User>(ConstantStrings.IndexName);
+
+            return Ok(response);
+        }
+
+        [HttpGet("GetStatsAge")]
+        public async Task<IActionResult> GetStatsAge()
+        {
+            Aggregation aggregation = new Aggregation();
+            var response = await aggregation.GetStatsValueAsync<User>(ConstantStrings.IndexName);
+
+            return Ok(response);
+        }
+    }
+}
