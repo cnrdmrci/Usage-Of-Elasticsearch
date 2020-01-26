4c60c8c8c0ec7ad0c9b3939a7ab5638adb824d32
cnrdmrci <canerdemirci.mail@gmail.com>
2020-01-26 23:02:34 +0300

    Query DSL classes are created.

---
+++ QueryDSL/TermLevelQueries/RangeQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Usage_Of_Elasticsearch.QueryDSL.TermLevelQueries
+{
+    public class RangeQuery
+    {
+    }
+}
