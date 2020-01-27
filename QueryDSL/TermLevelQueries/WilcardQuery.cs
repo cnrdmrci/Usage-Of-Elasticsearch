8a8a589241857a276602f66f74cf7a4a9f4be716
cnrdmrci <canerdemirci.mail@gmail.com>
2020-01-27 20:31:02 +0300

    Term level queries are completed.

--- QueryDSL/TermLevelQueries/WilcardQuery.cs
+++ QueryDSL/TermLevelQueries/WilcardQuery.cs
@@ -2,10 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Nest;
 
 namespace Usage_Of_Elasticsearch.QueryDSL.TermLevelQueries
 {
     public class WilcardQuery
     {
+        private readonly ElasticClient _elasticClient = ElasticInstanceCreator.GetElasticClientInstance();
+
+        public async Task<List<T>> SearchUserNameWithWildcardQuery<T>(string indexName, string wildcard) where T : class
+        {
+            var response = await _elasticClient.SearchAsync<T>(p => p
+                .Index(indexName)
+                .Query(q => q
+                    .Wildcard(w => w
+                        .Field("userName")
+                        .Value(wildcard))
+                )
+            );
+            return response.Documents.ToList();
+        }
+
+        public async Task<List<User>> SearchLastNameWithPrefixQuery(string indexName, string wildcard)
+        {
+            var response = await _elasticClient.SearchAsync<User>(p => p
+                .Index(indexName)
+                .Query(q => q
+                    .Wildcard(w => w
+                        .Field(f => f.UserName)
+                        .Value(wildcard))
+                )
+            );
+            return response.Documents.ToList();
+        }
     }
 }
