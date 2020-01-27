fd10cc6a3f4fecb538eb70da8ee8077ef828c5f4
cnrdmrci <canerdemirci.mail@gmail.com>
2020-01-27 21:06:51 +0300

    from, size and sort OK

---
+++ QueryDSL/OtherQueries/SortQuery.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nest;
+
+namespace Usage_Of_Elasticsearch.QueryDSL.OtherQueries
+{
+    public class SortQuery
+    {
+        private ElasticClient elasticClient = ElasticInstanceCreator.GetElasticClientInstance();
+
+        public async Task<User> SearchFromSizeQuery(string indexName, string userName)
+        {
+            var response = await elasticClient.SearchAsync<User>(p => p
+                .Index(indexName)
+                .From(0)
+                .Size(10)
+                .Query(q => q
+                    .MatchAll()
+                )
+                .Sort(s => s
+                    .Descending(d=>d.UserName)
+                )
+            );
+            return response.Documents.FirstOrDefault();
+        }
+    }
+}
