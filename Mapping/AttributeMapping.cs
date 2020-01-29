f783cc5d838ab725f2aec23e4deb285fb758ae4a
cnrdmrci <canerdemirci.mail@gmail.com>
2020-01-29 23:25:36 +0300

    mapping settings are added

---
+++ Mapping/AttributeMapping.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nest;
+
+namespace Usage_Of_Elasticsearch.Mapping
+{
+    public class AttributeMapping
+    {
+        private ElasticClient _elasticClient = ElasticInstanceCreator.GetElasticClientInstance();
+        public void Create()
+        {
+            var createIndexResponse = _elasticClient.Indices.Create("myindex", c => c
+                .Map<EmployeeA>(m => m.AutoMap())
+            );
+        }
+    }
+
+    [ElasticsearchType(RelationName = "employee")]
+    public class EmployeeA
+    {
+        [Text(Name = "first_name", Norms = false, Similarity = "LMDirichlet")]
+        public string FirstName { get; set; }
+
+        [Text(Name = "last_name")]
+        public string LastName { get; set; }
+
+        [Number(DocValues = false, IgnoreMalformed = true, Coerce = true)]
+        public int Salary { get; set; }
+
+        [Date(Format = "MMddyyyy")]
+        public DateTime Birthday { get; set; }
+
+        [Boolean(NullValue = false, Store = true)]
+        public bool IsManager { get; set; }
+
+        [Nested]
+        [PropertyName("empl")]
+        public List<EmployeeA> Employees { get; set; }
+
+        [Text(Name = "office_hours")]
+        public TimeSpan? OfficeHours { get; set; }
+
+        [Object]
+        public List<Skill> Skills { get; set; }
+    }
+
+    public class Skill
+    {
+        [Text]
+        public string Name { get; set; }
+
+        [Number(NumberType.Byte, Name = "level")]
+        public int Proficiency { get; set; }
+    }
+}
+
+/*
+ {
+  "mappings": {
+    "properties": {
+      "birthday": {
+        "format": "MMddyyyy",
+        "type": "date"
+      },
+      "empl": {
+        "properties": {},
+        "type": "nested"
+      },
+      "first_name": {
+        "type": "text",
+        "norms": false,
+        "similarity": "LMDirichlet"
+      },
+      "isManager": {
+        "null_value": false,
+        "store": true,
+        "type": "boolean"
+      },
+      "last_name": {
+        "type": "text"
+      },
+      "office_hours": {
+        "type": "text"
+      },
+      "salary": {
+        "coerce": true,
+        "doc_values": false,
+        "ignore_malformed": true,
+        "type": "float"
+      },
+      "skills": {
+        "properties": {
+          "level": {
+            "type": "byte"
+          },
+          "name": {
+            "type": "text"
+          }
+        },
+        "type": "object"
+      }
+    }
+  }
+}
+ */
---
+++ Mapping/FluentMapping.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nest;
+
+namespace Usage_Of_Elasticsearch.Mapping
+{
+    public class FluentMapping
+    {
+        private ElasticClient _elasticClient = ElasticInstanceCreator.GetElasticClientInstance();
+        public void Create()
+        {
+            var createIndexResponse = _elasticClient.Indices.Create("myindex", c => c
+                .Map<Company>(m => m
+                    .Properties(ps => ps
+                        .Text(s => s
+                            .Name(n => n.Name)
+                        )
+                        .Object<Employee>(o => o
+                            .Name(n => n.Employees)
+                            .Properties(eps => eps
+                                .Text(s => s
+                                    .Name(e => e.FirstName)
+                                    .Analyzer("my_analyzer")
+                                )
+                                .Text(s => s
+                                    .Name(e => e.LastName)
+                                )
+                                .Number(n => n
+                                    .Name(e => e.Salary)
+                                    .Type(NumberType.Integer)
+                                )
+                                .Date(d => d
+                                    .Name(n => n.Birthday)
+                                    .Format("dd-MM-yyyy"))
+                            )
+                        )
+                    )
+                )
+            );
+        }
+    }
+
+    public class Company
+    {
+        public string Name { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Salary { get; set; }
+        public DateTime Birthday { get; set; }
+        public bool IsManager { get; set; }
+        public List<Employee> Employees { get; set; }
+        public TimeSpan Hours { get; set; }
+    }
+}
