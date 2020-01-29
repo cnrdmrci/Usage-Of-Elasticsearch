6b347ffe994437aec7227714478c4518692653fe
cnrdmrci <canerdemirci.mail@gmail.com>
2020-01-29 21:46:24 +0300

    bool query added

--- QueryDSL/CompoundQueries/BoolQuery.cs
+++ QueryDSL/CompoundQueries/BoolQuery.cs
@@ -2,10 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Nest;
 
 namespace Usage_Of_Elasticsearch.QueryDSL.CompoundQueries
 {
     public class BoolQuery
     {
+        private readonly ElasticClient _elasticClient = ElasticInstanceCreator.GetElasticClientInstance();
+
+        public async Task<List<T>> SearchBoolQuery<T>(string indexName) where T : class
+        {
+            var response = await _elasticClient.SearchAsync<T>(p => p
+                .Index(indexName)
+                .Query(q => q
+                    .Bool(b => b
+                        .Must(m => m
+                            .Range(r => r
+                                .Field("age")
+                                .GreaterThan(20)
+                                .Relation(RangeRelation.Within)
+                            )
+                        ).MustNot(mn => mn
+                            .Match(mt => mt
+                                .Field("userName")
+                                .Query("caner")
+                            )
+                        ).Should(sh => sh
+                            .MultiMatch(mm => mm
+                                .Query("demirci")
+                                .Fields(fds => fds
+                                    .Field("FirstName")
+                                    .Field("lastName")
+                                )
+                            )
+                        ).Filter(flt => flt
+                            .Term(tr => tr
+                                .Field("gender")
+                                .Value("M")))
+                    )
+                )
+            );
+            return response.Documents.ToList();
+        }
     }
 }
